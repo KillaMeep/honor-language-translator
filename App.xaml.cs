ddcf1955e5dfb70955939e184b0a0f6124d99266
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-11-15 04:25:04 -0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfTranslator;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace WpfTranslator
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string hon = " !;\"#÷$%&>*()?,[]+=@/:x'-_";
+        private readonly string eng = " abcdefghijklmnopqrstuvwxyz";
+
+        private readonly HashSet<char> engChars;
+        private readonly HashSet<char> honChars;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            engChars = new HashSet<char>(eng);
+            honChars = new HashSet<char>(hon);
+        }
+
+        private string TranslateTo(string translate)
+        {
+            return new string(translate
+                .Where(chars => engChars.Contains(chars))
+                .Select(chars => hon[eng.IndexOf(chars)])
+                .ToArray());
+        }
+
+        private string TranslateFrom(string translate)
+        {
+            return new string(translate
+                .Where(chars => honChars.Contains(chars))
+                .Select(chars => eng[hon.IndexOf(chars)])
+                .ToArray());
+        }
+
+        private void TranslateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string input = InputTextBox.Text?.ToLower();
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                string result = AutoTranslate(input);
+                ResultTextBox.Text = result;
+            }
+            else
+            {
+                ResultTextBox.Text = "Invalid input.";
+            }
+        }
+
+        private string AutoTranslate(string inputString)
+        {
+            foreach (char c in inputString.Distinct())
+            {
+                if (engChars.Contains(c))
+                {
+                    TranslationLabel.Content = "Translated to Honor Language:";
+                    return TranslateTo(inputString);
+                }
+                else if (honChars.Contains(c))
+                {
+                    TranslationLabel.Content = "Translated from Honor Language:";
+                    return TranslateFrom(inputString);
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
